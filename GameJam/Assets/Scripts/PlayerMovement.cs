85e252aee66dc35a401df1b261d94eb9f5aef7dd
DictatorTodd <makana.hellberg@gmail.com>
2024-12-23 03:02:33 -0800

    Acceleration movement
    added acceleration and deceleration

--- GameJam/Assets/Scripts/PlayerMovement.cs
+++ GameJam/Assets/Scripts/PlayerMovement.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 25f;
+    public float speed = 10;
     private CharacterController player1;
     private Vector3 inputVector;
     private Vector3 movementVector;
     private float gravity = -10f;
+    public float acceleration = .25f;
 
     // Start is called before the first frame update
     void Start()
@@ -24,16 +25,23 @@ public class PlayerMovement : MonoBehaviour
     }
     void GetInput()
     {
-        inputVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        inputVector.Normalize();
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            inputVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+            inputVector.Normalize();
+            Vector3 forward = transform.forward;
+            Vector3 right = transform.right;
+            forward.y = 0;
+            right.y = 0;
+            forward.Normalize();
+            right.Normalize();
+            inputVector = (forward * inputVector.z + right * inputVector.x).normalized;
+        }
+        else
+        {
+            inputVector = Vector3.Lerp(inputVector, Vector3.zero, acceleration * Time.deltaTime);
+        }
 
-        Vector3 forward = transform.forward;
-        Vector3 right = transform.right;
-        forward.y = 0;
-        right.y = 0;
-        forward.Normalize();
-        right.Normalize();
-        inputVector = (forward * inputVector.z + right * inputVector.x).normalized;
         movementVector = (inputVector * speed) + (Vector3.up * gravity);
 
         //inputVector = transform.TransformDirection(inputVector);
@@ -41,6 +49,7 @@ public class PlayerMovement : MonoBehaviour
     }
     void MovePlayer()
     {
+
         player1.Move((Vector3)movementVector * Time.deltaTime);
     }
 }
